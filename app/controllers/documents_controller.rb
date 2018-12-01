992e1b603975c771aaaa17d5e01d31acc178c217
= <=>
2018-12-01 20:46:08 +0100

    Document controller and routes

---
+++ app/controllers/documents_controller.rb
@@ -0,0 +1,31 @@
+class DocumentsController < ApplicationController
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def set_document
+  end
+
+  def document_params
+  end
+end
