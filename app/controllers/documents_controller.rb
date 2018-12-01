41b205669dbe3f4c69942a86c79705e78a7c55cf
= <=>
2018-12-01 21:15:06 +0100

    Added show and index view templetes

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,15 +1,27 @@
 class DocumentsController < ApplicationController
+  before_action :set_document, only: [:show, :edit, :update, :destroy]
 
   def index
+    @documents = Document.all.order("created_at DESC")
   end
 
   def show
   end
 
   def new
+    @document = Document.new
   end
 
   def create
+    @document = Document.new(document_params)
+
+    respond_to do |format|
+      if @document.save
+        format.html { redirect_to @document, notice: 'Document was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
   end
 
   def edit
@@ -24,8 +36,10 @@ class DocumentsController < ApplicationController
   private
 
   def set_document
+    @document = Document.find(params[:id])
   end
 
   def document_params
+    params.require(:document).permit(:title, :content)
   end
 end
